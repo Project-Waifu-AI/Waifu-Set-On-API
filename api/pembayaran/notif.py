f4fbd9977faaf4ac45c6b5b3203987a90a98b9bb
DimasGodim <dimas.ngadinegaran@gmail.com>
2024-04-20 05:51:47 +0700

    'update'

--- api/pembayaran/notif.py
+++ api/pembayaran/notif.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter, Request
 
 router = APIRouter(prefix='/pembayaran', tags=['pembayaran-wso'])
 
+@router.post('/notification')
+def notif(req: Request):
+    print(str(req))
+
+
 @router.get('/notification')
 def notif(req: Request):
     print(str(req))
