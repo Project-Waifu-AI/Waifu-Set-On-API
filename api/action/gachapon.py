19dd6f0911e523615db48097bcaae4dbd813d7e2
Ragil1305 <ragil05@gmail.com>
2024-05-16 08:02:33 +0700

    mengubah autentikasi endpoint

--- api/action/gachapon.py
+++ api/action/gachapon.py
@@ -1,11 +1,20 @@
-from fastapi import APIRouter, Depends, Header, HTTPException
-from helper.gacha_logic import gacha_pull, get_user_data, get_gacha_history, get_current_user
-from database.model import KarakterData, userdata
+from fastapi import APIRouter, Header, HTTPException
+from fastapi.responses import RedirectResponse, JSONResponse
+from helper.gacha_logic import gacha_pull, get_user_data, get_gacha_history
+from helper.access_token import check_access_token_expired, decode_access_token
+from configs import config
 
 router = APIRouter(prefix='/gacha', tags=['gachapon system'])
 
 @router.get("/karakteryangdimiliki")
-async def get_obtained_characters(email: str = Depends(get_current_user)):
+async def get_obtained_characters(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+
     user_data = await get_user_data(email)
     if user_data:
         return {"karakterYangDimiliki": user_data.karakterYangDimiliki}
@@ -13,33 +22,68 @@ async def get_obtained_characters(email: str = Depends(get_current_user)):
         return {"message": "User not found"}
     
 @router.get("/gacha/single/non-limited")
-async def gacha_single_non_limited(email: str = Depends(get_current_user)):
+async def gacha_single_non_limited(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+        
     email = email
     results = await gacha_pull(email, 1, limited=False)
     return [char.nama for char in results]
 
 @router.get("/gacha/multi/non-limited")
-async def gacha_multi_non_limited(email: str = Depends(get_current_user)):
+async def gacha_multi_non_limited(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+        
     email = email
     results = await gacha_pull(email, 10, limited=False)
     return [char.nama for char in results]
     
 
 @router.get("/gacha/single/limited")
-async def gacha_single_limited(email: str = Depends(get_current_user)):
+async def gacha_single_limited(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+        
     email = email
     results = await gacha_pull(email, 1, limited=True)
     return [char.nama for char in results]
     
 
 @router.get("/gacha/multi/limited")
-async def gacha_multi_limited(email: str = Depends(get_current_user)):
+async def gacha_multi_limited(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+        
     email = email
     results = await gacha_pull(email, 10, limited=True)
     return [char.nama for char in results]
 
 
 @router.get("/gacha/history")
-async def gacha_history(email: str = Depends(get_current_user)):
+async def gacha_history(access_token: str = Header(...)):
+    check = check_access_token_expired(access_token=access_token)
+    if check is True:
+        return RedirectResponse(url=config.redirect_uri_page_masuk, status_code=401)
+    elif check is False:
+        payloadJWT = decode_access_token(access_token=access_token)
+        email = payloadJWT.get('sub')
+        
     history = await get_gacha_history(email)
     return history
